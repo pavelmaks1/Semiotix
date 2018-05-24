f2abb6998e9a3da326cd18f5315ddcc4422b6003
pavelmaks1 <pavel.maks7@yandex.ru>
2018-05-24 13:42:26 +0300

    Added icons to questions

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@ answer.map(element =>
         event.preventDefault();
         element.childNodes[1].classList.toggle('active');
         element.childNodes[3].classList.toggle('display');
-        
-        // item.classList.toggle('display');
+        console.log(element.childNodes[1].childNodes[3].classList.toggle('fa-angle-down'))
+        console.log(element.childNodes[1].childNodes[3].classList.toggle('fa-angle-up'))
     }))
