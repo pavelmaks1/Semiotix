25f38ff7db1623558350eb2d0b581b2673db6e51
pavelmaks1 <pavel.maks7@yandex.ru>
2018-05-25 18:46:46 +0300

    Fixed

--- js/script.js
+++ js/script.js
@@ -8,4 +8,11 @@ answer.map(element =>
         element.childNodes[3].classList.toggle('display');
         console.log(element.childNodes[1].childNodes[3].classList.toggle('fa-angle-down'))
         console.log(element.childNodes[1].childNodes[3].classList.toggle('fa-angle-up'))
+        if (element.childNodes[1].childNodes[3].attributes.src.value === "images/icon-arrow.svg") {
+            element.childNodes[1].childNodes[3].attributes.src.value = "images/icon-arrow-up.svg";
+        } else if (element.childNodes[1].childNodes[3].attributes.src.value === "images/icon-arrow-up.svg") {
+            element.childNodes[1].childNodes[3].attributes.src.value = "images/icon-arrow.svg"
+        }
+        console.log(element.childNodes[1].childNodes[3].attributes.src)
+
     }))
